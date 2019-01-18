59c8a408a01c642c9046232ed5cbc3dcab89bcb1
Najja <nkossally@gmail.com>
2019-01-18 08:03:55 -0500

    Added user model

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class user < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+
+  attr_reader :password
+
+  has_many :polls
+  has_many :groups
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+end
