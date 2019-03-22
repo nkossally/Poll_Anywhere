79f30d32897f7c98258153c757193a4fd8e438cc
Najja <nkossally@gmail.com>
2019-03-22 04:29:42 -0400

    Change activate icon to text.

--- frontend/components/users/user.jsx
+++ frontend/components/users/user.jsx
@@ -5,7 +5,7 @@ import BlueNavBar from "../nav_bar/blue_nav_bar_container";
 class User extends React.Component {
   constructor(props) {
     super(props);
-    this.state={ selectedPolls: [], showModal: false, draggedPoll: "" };
+    this.state = { selectedPolls: [], showModal: false, draggedPoll: "" };
     this.activate = this.activate.bind(this);
     this.selectPoll = this.selectPoll.bind(this);
     this.handleModal = this.handleModal.bind(this);
@@ -21,132 +21,173 @@ class User extends React.Component {
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.showUser(this.props.user.id);
     this.props.showAllPolls();
     this.props.showAllGroups();
   }
 
-  onDrag(event, poll){
+  onDrag(event, poll) {
     event.preventDefault();
     this.setState({
       draggedPoll: poll
     });
   }
 
-  onDragOver(event){
+  onDragOver(event) {
     event.preventDefault();
   }
 
-  onDrop(event, group ){
+  onDrop(event, group) {
     event.preventDefault();
-    this.props.updatePollChangeGroup( [this.state.draggedPoll.id], group)
+    this.props.updatePollChangeGroup([this.state.draggedPoll.id], group);
   }
 
-  handleUngroup(){
-    let pollIds = this.state.selectedPolls.map(poll=>poll.id);
+  handleUngroup() {
+    let pollIds = this.state.selectedPolls.map(poll => poll.id);
     let group = this.props.groups[0];
     this.props.updatePollChangeGroup(pollIds, group);
     this.setState({ selectedPolls: [] });
   }
 
-  handleModalContainer(){
+  handleModalContainer() {
     this.handleModal();
     this.props.showAllGroups();
     this.props.showAllPolls();
   }
 
-  handleModal(){
+  handleModal() {
     this.props.openModal(this.props.user.id, this.state.selected_polls);
   }
 
-  selectPoll(poll){
-    return()=>{
+  selectPoll(poll) {
+    return () => {
       this.props.selectPoll(poll);
       let newSelection = this.state.selectedPolls;
       newSelection.push(poll);
-      this.setState({selectedPolls: newSelection});
-    }
+      this.setState({ selectedPolls: newSelection });
+    };
   }
 
-  activate(id){
-    return()=>{
-      let inactivePoll = {active: false};
-      let activePoll = {active: true};
-      for(let i=0; i<this.props.polls.length; i++){
+  activate(id) {
+    return () => {
+      let inactivePoll = { active: false };
+      let activePoll = { active: true };
+      for (let i = 0; i < this.props.polls.length; i++) {
         const poll = this.props.polls[i];
 
-        if(poll.id === id){
-          this.props.updatePoll(activePoll, poll.id)
+        if (poll.id === id) {
+          this.props.updatePoll(activePoll, poll.id);
         } else {
-          this.props.updatePoll(inactivePoll, poll.id)
+          this.props.updatePoll(inactivePoll, poll.id);
         }
       }
-    } 
+    };
   }
 
   render() {
- 
-    let groupsAndPolls = this.props.groups.map(group=>{
-      return(
-        <div key={group.id} onDrop={(event) => this.onDrop(event, group)} onDragOver={(event => this.onDragOver(event)) } >
-          <li className="group-title" >{group.title}</li>
-          <ul className="group-list"   >
-            {this.props.polls.filter(poll => poll.group_id === group.id).map((poll, idx)=>{
-              let className;
-              if(poll.active){
-                className = "green-user-single-poll";
-              } else {
-                className = "user-single-poll";
-              }
-              return(
-                <div className={className} key={idx} draggable
-                onDrag={(event) => this.onDrag(event, poll)}
-                > 
-                <div className={ poll.active ? "dark-green-div" : "user-empty-div"}> </div>
-
-                  <div className="check-and-text">
-                    <div className="check-container">
-                      <input type="checkbox" onClick={this.selectPoll(poll)}/>
+    let groupsAndPolls = this.props.groups.map(group => {
+      return (
+        <div
+          key={group.id}
+          onDrop={event => this.onDrop(event, group)}
+          onDragOver={event => this.onDragOver(event)}
+        >
+          <li className="group-title">{group.title}</li>
+          <ul className="group-list">
+            {this.props.polls
+              .filter(poll => poll.group_id === group.id)
+              .map((poll, idx) => {
+                let className;
+                if (poll.active) {
+                  className = "green-user-single-poll";
+                } else {
+                  className = "user-single-poll";
+                }
+                return (
+                  <div
+                    className={className}
+                    key={idx}
+                    draggable
+                    onDrag={event => this.onDrag(event, poll)}
+                  >
+                    <div
+                      className={
+                        poll.active ? "dark-green-div" : "user-empty-div"
+                      }
+                    >
+                      {" "}
                     </div>
-                    <Link to={`/polls/${poll.id}`}><div className="poll-text">{poll.body}</div></Link>
-                  </div>
-                  <Link className="user-edit-poll" to={`/polls/${poll.id}/edit`}>Edit</Link>
-                  <button onClick={this.activate(poll.id)}>
-                    <i className="fas fa-toggle-on" id="user-activate-icon"></i>
-                  </button>
 
-                </div>
-                )
-            })}
+                    <div className="check-and-text">
+                      <div className="check-container">
+                        <input
+                          type="checkbox"
+                          onClick={this.selectPoll(poll)}
+                        />
+                      </div>
+                      <Link to={`/polls/${poll.id}`}>
+                        <div className="poll-text">{poll.body}</div>
+                      </Link>
+                    </div>
+                    <Link
+                      className="user-edit-poll"
+                      to={`/polls/${poll.id}/edit`}
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      className="activate-text"
+                      onClick={this.activate(poll.id)}
+                    >
+                      Activate
+                    </button>
+                  </div>
+                );
+              })}
           </ul>
-          <div className="white-space"></div>
+          <div className="white-space" />
         </div>
-      )
-      
-    })
+      );
+    });
     return (
       <div>
         <BlueNavBar />
         <div className="user-container">
-            <div className="user-create-button">
-            <Link to="/createpoll" id="extend-button">    Create</Link>
-            </div>
+          <div className="user-create-button">
+            <Link to="/createpoll" id="extend-button">
+              {" "}
+              Create
+            </Link>
+          </div>
           <ul className="user-polls">
             <li className="user-polls-header">
-              <button disabled={ this.state.selectedPolls.length === 0 } className={ this.state.selectedPolls.length === 0 ? "disabled" : "" } onClick={this.handleModal}>Group</button>
-              <button disabled={ this.state.selectedPolls.length === 0 } className={ this.state.selectedPolls.length === 0 ? "disabled" : "" } onClick={this.handleUngroup}>Ungroup</button>
+              <button
+                disabled={this.state.selectedPolls.length === 0}
+                className={
+                  this.state.selectedPolls.length === 0 ? "disabled" : ""
+                }
+                onClick={this.handleModal}
+              >
+                Group
+              </button>
+              <button
+                disabled={this.state.selectedPolls.length === 0}
+                className={
+                  this.state.selectedPolls.length === 0 ? "disabled" : ""
+                }
+                onClick={this.handleUngroup}
+              >
+                Ungroup
+              </button>
             </li>
 
             {groupsAndPolls}
-
           </ul>
-    
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default User;
